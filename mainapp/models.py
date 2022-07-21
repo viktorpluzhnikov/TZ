de30c45ebc4129d80efe91f9902e0e53288e987e
majahed228 <pluzhnikov93@inbox.ru>
2022-07-21 23:18:31 +0300

    api

--- mainapp/models.py
+++ mainapp/models.py
@@ -4,10 +4,13 @@ from uuid import uuid4
 
 
 class Offers(models.Model):
-    # uid = models.UUIDField(primary_key=True, default=uuid4)
-    name = models.CharField('name', max_length=256)
-    price = models.IntegerField()
-    size = models.IntegerField('param2_text')
+    offer_id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=256, blank=True, null=True)
+    price = models.FloatField(blank=True, null=True)
+    param2_text = models.IntegerField(blank=True, null=True)
 
-    def __str__(self):
-        return self.name
+
+    class Meta:
+        managed = False
+        db_table = 'offers'
+
